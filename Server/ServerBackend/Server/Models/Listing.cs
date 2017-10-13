2b2ff62347f845b501498b7bfb50c5f9cd6666e9
Josh Kostal <joshkostal4@gmail.com>
2017-10-13 13:31:59 -0500

    Filling in Listing

--- Server/ServerBackend/Server/Models/Listing.cs
+++ Server/ServerBackend/Server/Models/Listing.cs
@@ -1,1 +1,21 @@
+using System;
 
+namespace Server.Models
+{
+    public class Listing
+    {
+        public int Price { get; set; }
+
+        public enum ConditionTypes { Great, Good, Poor}
+
+        public ConditionTypes Condition { get; set; }
+
+        public DateTime LastDateEdited { get; set; }
+
+        public string ISBN { get; set; }
+
+        public enum ListingTypes { Sell, Buy }
+
+        public ListingTypes ListingType { get; set; }
+    }
+}
