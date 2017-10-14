cbd72b6d0a0798a9946646cc5236e7931b25a675
Josh Kostal <joshkostal4@gmail.com>
2017-10-13 22:28:49 -0500

    Create database connection and sample queries. Add constructors to objects.

--- Server/ServerBackend/Server/Models/Book.cs
+++ Server/ServerBackend/Server/Models/Book.cs
@@ -4,6 +4,12 @@ namespace Server.Models
 {
     public class Book
     {
+        public Book(string ISBN, List<string> listingsWithBook)
+        {
+            ISBN = ISBN;
+            ListingsWithBook = listingsWithBook;
+        }
+
         public string ISBN { get; set; }
 
         public List<string> ListingsWithBook { get; set; }
--- Server/ServerBackend/Server/Models/User.cs
+++ Server/ServerBackend/Server/Models/User.cs
@@ -4,6 +4,15 @@ namespace Server.Models
 {
     public class User
     {
+        public User CreateUser(string Username, string FirstName, string LastName, string PrimaryEmail, string SecondaryEmail, Password UserPassword)
+        {
+            Username = Username;
+            FirstName = FirstName;
+            LastName = LastName;
+            PrimaryEmail = PrimaryEmail;
+            SecondaryEmail = SecondaryEmail;
+            UserPassword = UserPassword;
+        }
         public string UserName { get; set; }
 
         public string FirstName { get; set; }
@@ -13,6 +22,8 @@ namespace Server.Models
         public string PrimaryEmail { get; set; }
 
         public string SecondaryEmail { get; set; }
+
+        public Password UserPassword { get; set; }
 
         public class Password
         {
