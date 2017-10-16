6169a189122d82a44b2a854006b470c86fbd0e4f
Josh Kostal <joshkostal4@gmail.com>
2017-10-15 20:33:50 -0500

    Reset password email added

--- Server/ServerBackend/Server/Models/Email.cs
+++ Server/ServerBackend/Server/Models/Email.cs
@@ -12,7 +12,7 @@ namespace Server.Models
             MailMessage mail = new MailMessage();
 
             mail.From = new MailAddress("CookieNapServices@gmail.com");
-            mail.To.Add(recipient.SecondaryEmail);
+            mail.To.Add(recipient.CommunicationEmail);
             mail.Subject = "Cookie Nap Account Registration";
 
             string confirmationCode = Guid.NewGuid().ToString("n").Substring(0, 8);
@@ -32,10 +32,27 @@ namespace Server.Models
             MailMessage mail = new MailMessage();
 
             mail.From = new MailAddress("CookieNapServices@gmail.com");
-            mail.To.Add(recipient.SecondaryEmail);
+            mail.To.Add(recipient.CommunicationEmail);
             mail.Subject = "A User Has Requested Your Contact Information";
 
-            mail.Body = string.Format("A user has requested your contact information after viewing one of your listings. <a href='url'>Please click here to allow this user to view your email address.</a> Otherwise, you may ignore this message.");
+            mail.Body = string.Format("A user has requested your contact information after viewing one of your listings. <a href='url'>Please click here to allow this user to view your email address.</a> Otherwise, you may ignore this message.");  //enter correct url when ready
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient("smtp.google.com", 587);
+            smtp.Credentials = new NetworkCredential("CookieNapServices@gmail.com", "c00kienap");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+        public string ResetPasswordEmail(User recipient)
+        {
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("CookieNapServices@gmail.com");
+            mail.To.Add(recipient.CommunicationEmail);
+            mail.Subject = "Reset Your Cookie Nap Password";
+
+            mail.Body = "You have requested to have your password reset. <a href='url'>Please click here to reset the password.</a>";   //enter correct url when ready
             mail.IsBodyHtml = true;
 
             SmtpClient smtp = new SmtpClient("smtp.google.com", 587);
