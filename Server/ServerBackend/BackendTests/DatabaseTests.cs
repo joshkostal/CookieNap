4a5ff5beb509055fe85d9de70eac1698be95bce4
Josh Kostal <joshkostal4@gmail.com>
2017-10-27 12:05:36 -0500

    Minor changes to tests

--- Server/ServerBackend/BackendTests/DatabaseTests.cs
+++ Server/ServerBackend/BackendTests/DatabaseTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Server.Models;
 using System;
+using System.Collections.Generic;
 
 namespace BackendTests
 {
@@ -47,10 +48,11 @@ namespace BackendTests
         }
 
         [TestMethod]
-        public void InsertGetAndDeleteBListingTest()
+        public void InsertGetAndDeleteListingTest()
         {
             //Arrange
-            user.UserID = 1;
+            User newUser = user;
+            newUser.UserID = 1;
             listing = _dbc.InsertListing(listing);
 
             //Act
@@ -76,6 +78,7 @@ namespace BackendTests
 
             //Tear Down
             _dbc.DeleteListingByID(listing.ListingID);
+            listing.Price = 5;
 
             //Assert
             Assert.AreEqual(listing.Price, updatedListing.Price);
@@ -104,20 +107,21 @@ namespace BackendTests
         public void DeleteListingsPastDeletionDateTest()
         {
             //Arrange
-            listing.LastDateEdited = new DateTime(2000, 1, 1);
-            listing = _dbc.InsertListing(listing);
+            Listing newListing = listing;
+            newListing.LastDateEdited = new DateTime(2000, 1, 1);
+            newListing = _dbc.InsertListing(newListing);
 
             //Act
             _dbc.DeleteListingsPastDeletionDate();
 
-            var id = listing.ListingID;
-            listing = _dbc.GetListing(id);
+            var id = newListing.ListingID;
+            newListing = _dbc.GetListing(id);
 
             //Tear Down
             _dbc.DeleteListingByID(id);
 
             //Assert
-            Assert.IsNull(listing);
+            Assert.IsNull(newListing);
         }
 
         [TestMethod]
@@ -139,15 +143,56 @@ namespace BackendTests
         public void FindBooksListedTest()
         {
             //Arrange
+            List<Book> booksListed = new List<Book>();
 
+            Book book1 = new Book("1111111111");
+            Listing listing1 = listing;
+            listing1.BookListed = book1;
+            booksListed.Add(book1);
+
+            Book book2 = new Book("2222222222");
+            Listing listing2 = listing;
+            listing1.BookListed = book2;
+            booksListed.Add(book2);
+
+            Book book3 = new Book("3333333333");
+            Listing listing3 = listing;
+            listing1.BookListed = book3;
+            booksListed.Add(book3);
+
+            Book book4 = new Book("4444444444");
+            Listing listing4 = listing;
+            listing1.BookListed = book4;
+            booksListed.Add(book4);
+
+            Listing listing5 = listing;
+            listing1.BookListed = book4;
+            booksListed.Add(book4);
+
+            Listing listing6= listing;
+            listing1.BookListed = book4;
+            booksListed.Add(book4);
 
             //Act
+            listing1 = _dbc.InsertListing(listing1);
+            listing2 = _dbc.InsertListing(listing2);
+            listing3 = _dbc.InsertListing(listing3);
+            listing4 = _dbc.InsertListing(listing4);
+            listing5 = _dbc.InsertListing(listing5);
+            listing6 = _dbc.InsertListing(listing6);
 
-
-            //Assert
-
+            List<Book> foundBooks = _dbc.FindBooksListed();
 
             //Tear Down
+            _dbc.DeleteListingByID(listing1.ListingID);
+            _dbc.DeleteListingByID(listing2.ListingID);
+            _dbc.DeleteListingByID(listing3.ListingID);
+            _dbc.DeleteListingByID(listing4.ListingID);
+            _dbc.DeleteListingByID(listing5.ListingID);
+            _dbc.DeleteListingByID(listing6.ListingID);
+
+            //Assert
+            Assert.AreEqual(foundBooks.Count, 4);
         }
 
         [TestMethod]
